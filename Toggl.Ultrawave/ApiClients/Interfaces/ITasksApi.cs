b335b5c42c87cf2cf39678ba82416a939934d7ef
Artūrs Eimanis <arturseimanis@gmail.com>
2017-06-13 10:50:04 +0300

    :construction: Fix Task namespace in TasksApi

--- Toggl.Ultrawave/ApiClients/Interfaces/ITasksApi.cs
+++ Toggl.Ultrawave/ApiClients/Interfaces/ITasksApi.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
+using Toggl.Ultrawave.Models;
 
 namespace Toggl.Ultrawave.ApiClients
 {
--- Toggl.Ultrawave/ApiClients/TasksApi.cs
+++ Toggl.Ultrawave/ApiClients/TasksApi.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
+using Toggl.Ultrawave.Models;
 using Toggl.Ultrawave.Network;
 using Toggl.Ultrawave.Serialization;
 
