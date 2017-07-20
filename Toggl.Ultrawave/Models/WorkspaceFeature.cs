5806c6f0200e658dc8b688413e76250391ab900b
Kornelije Petak <kpetak@koncar-inem.hr>
2017-07-20 10:57:25 +0200

    :sparkles: Add workspace features model

---
+++ Toggl.Ultrawave/Models/WorkspaceFeature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+using Toggl.Multivac;
+using Toggl.Multivac.Models;
+
+namespace Toggl.Ultrawave.Models
+{
+    public sealed class WorkspaceFeature : IWorkspaceFeature
+    {
+        // TODO: This property doesn't have a representation in Toggl API
+        // So WorkspaceFeature either can't implement IBaseModel or a surrogate implementation must be found
+        public int Id { get; set; }
+
+        public int WorkspaceId { get; set; }
+
+        public WorkspaceFeatureId FeatureId { get; set; }
+
+        public bool Enabled { get; set; }
+    }
+}
